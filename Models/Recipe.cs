e023b09766737bd3110bdb1663fdc3b993f255c7
anntnzrb <anntnzrb@proton.me>
2025-04-21 16:35:51 -0500

    feat: add Recipe model

---
+++ Models/Recipe.cs
@@ -0,0 +1,10 @@
+namespace asp_demo.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public List<RecipeIngredient>? Ingredients { get; set; } = [];
+    }
+}
